0c96b4bc0853eb0b64f09690ad421b5851f15bf8
David Cottrell <davidjcottrell@icloud.com>
2025-01-03 16:32:52 +0000

    Add custom key and mouse codes

--- include/ArnoldEngine.h
+++ include/ArnoldEngine.h
@@ -5,6 +5,11 @@
 #include "Graphics/UI/ImGuiLayer.h"
 #include "Core/Log.h"
 
+// --- Input ---
+#include "Core/Input.h"
+#include "Core/KeyCodes.h"
+#include "Core/MouseButtonCodes.h"
+
 // Only include entry point in the main application file
 #ifdef AE_MAIN
 #include "Core/EntryPoint.h"
---
+++ src/Core/MouseButtonCodes.h
@@ -0,0 +1,16 @@
+#pragma once
+
+// From glfw3.h
+
+#define AE_BUTTON_1         0
+#define AE_BUTTON_2         1
+#define AE_BUTTON_3         2
+#define AE_BUTTON_4         3
+#define AE_BUTTON_5         4
+#define AE_BUTTON_6         5
+#define AE_BUTTON_7         6
+#define AE_BUTTON_8         7
+#define AE_BUTTON_LAST      AE_BUTTON_8
+#define AE_BUTTON_LEFT      AE_BUTTON_1
+#define AE_BUTTON_RIGHT     AE_BUTTON_2
+#define AE_BUTTON_MIDDLE    AE_BUTTON_3
