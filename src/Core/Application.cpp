74cd33dcc031fdce0bca3507f561604da11e8b3b
David Cottrell <davidjcottrell@icloud.com>
2025-01-08 13:55:40 +0000

    Update cmake to match new submodule name

--- src/Core/Application.cpp
+++ src/Core/Application.cpp
@@ -4,12 +4,12 @@
 #include <glad/glad.h>
 
 #include "Window.h"
-#include "../../external/sentinel/src/ConfigurationManager.h"
+#include "ConfigurationManager.h"
 #include "Events/ApplicationEvent.h"
 
 namespace AE::Core
 {
-    Application* Application::s_Instance = nullptr;
+    Application *Application::s_Instance = nullptr;
 
     Application::Application()
     {
@@ -21,13 +21,12 @@ namespace AE::Core
 
         using namespace Sentinel;
         constexpr ConfigurationParameters params =
-        {
-            .enableHotReload = true,
-            .validateOnLoad = true,
-            .fileReaderType = SENTINEL
-        };
+            {
+                .enableHotReload = true,
+                .validateOnLoad = true,
+                .fileReaderType = SENTINEL};
         ConfigurationManager::Initialize("config.sen", params);
-        const auto* config = ConfigurationManager::Get();
+        const auto *config = ConfigurationManager::Get();
 
         AE_CORE_INFO("Configuration value: {0}", config->GetConfigValue());
     }
@@ -43,14 +42,14 @@ namespace AE::Core
             glClear(GL_COLOR_BUFFER_BIT);
 
             // Update each layer
-            for (Layer* layer : m_LayerStack)
+            for (Layer *layer : m_LayerStack)
                 layer->OnUpdate();
 
             m_Window->OnUpdate();
         }
     }
 
-    void Application::OnEvent(Events::Event& e)
+    void Application::OnEvent(Events::Event &e)
     {
         Events::EventHandler handler(e);
         handler.TryHandle<Events::WindowCloseEvent>(AE_BIND_EVENT_FN(Application::OnWindowClose));
@@ -63,19 +62,19 @@ namespace AE::Core
         }
     }
 
-    void Application::PushLayer(Layer* layer)
+    void Application::PushLayer(Layer *layer)
     {
         m_LayerStack.PushLayer(layer);
         layer->OnAttach();
     }
 
-    void Application::PushOverlay(Layer* overlay)
+    void Application::PushOverlay(Layer *overlay)
     {
         m_LayerStack.PushOverlay(overlay);
         overlay->OnAttach();
     }
 
-    bool Application::OnWindowClose(Events::WindowCloseEvent& e)
+    bool Application::OnWindowClose(Events::WindowCloseEvent &e)
     {
         AE_CORE_INFO("ArnoldEngine closing...");
         m_Running = false;
