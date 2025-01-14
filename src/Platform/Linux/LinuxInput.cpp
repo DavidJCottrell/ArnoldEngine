348abf082533676953d320ca5ef587aa3a179257
David Cottrell <davidjcottrell@icloud.com>
2025-01-14 17:39:25 +0000

    Add missing methods to LinuxWindow and LinuxInput

--- src/Platform/Linux/LinuxInput.cpp
+++ src/Platform/Linux/LinuxInput.cpp
@@ -3,7 +3,7 @@
 #include "Core/Application.h"
 #include "GLFW/glfw3.h"
 
-AE::Core::Input* AE::Core::Input::s_Instance = new Platform::Linux::LinuxInput();
+AE::Core::Input *AE::Core::Input::s_Instance = new Platform::Linux::LinuxInput();
 
 namespace AE::Platform::Linux
 {
@@ -13,14 +13,14 @@ namespace AE::Platform::Linux
 
     bool LinuxInput::IsKeyPressedImpl(const int keycode)
     {
-        const auto window = static_cast<GLFWwindow*>(Core::Application::Get().GetWindow().GetNativeWindow());
+        const auto window = static_cast<GLFWwindow *>(Core::Application::Get().GetWindow().GetNativeWindow());
         const auto state = glfwGetKey(window, keycode);
         return state == GLFW_PRESS || state == GLFW_REPEAT;
     }
 
     bool LinuxInput::IsMouseButtonPressedImpl(const int button)
     {
-        const auto window = static_cast<GLFWwindow*>(Core::Application::Get().GetWindow().GetNativeWindow());
+        const auto window = static_cast<GLFWwindow *>(Core::Application::Get().GetWindow().GetNativeWindow());
         const auto state = glfwGetMouseButton(window, button);
         return state == GLFW_PRESS;
     }
@@ -39,7 +39,7 @@ namespace AE::Platform::Linux
 
     std::pair<float, float> LinuxInput::GetMousePositionImpl()
     {
-        const auto window = static_cast<GLFWwindow*>(Core::Application::Get().GetWindow().GetNativeWindow());
+        const auto window = static_cast<GLFWwindow *>(Core::Application::Get().GetWindow().GetNativeWindow());
         double xpos, ypos;
         glfwGetCursorPos(window, &xpos, &ypos);
         return {xpos, ypos};
--- src/Platform/Linux/LinuxWindow.cpp
+++ src/Platform/Linux/LinuxWindow.cpp
@@ -1,7 +1,24 @@
+#include "aepch.h"
+
 #include "LinuxWindow.h"
+
+#include <Core/Input.h>
+#include <Core/KeyCodes.h>
+#include <glad/glad.h>
+
+#include "Events/ApplicationEvent.h"
+#include "Events/MouseEvent.h"
+#include "Events/KeyEvent.h"
 
 namespace AE::Platform::Linux
 {
+    static bool s_GLFWInitialized = false;
+
+    static void GLFWErrorCallback(int error, const char *description)
+    {
+        AE_CORE_ERROR("GLFW Error ({0}): {1}", error, description);
+    }
+
     LinuxWindow::LinuxWindow(const WindowProps &props)
     {
         Init(props);
@@ -12,9 +29,128 @@ namespace AE::Platform::Linux
         Shutdown();
     }
 
-    void LinuxWindow::Shutdown()
+    void LinuxWindow::Shutdown() const
     {
         glfwDestroyWindow(m_Window);
+    }
+
+    void LinuxWindow::Init(const WindowProps &props)
+    {
+        AE_CORE_INFO("Initialising window...");
+
+        m_Data.Title = props.title;
+        m_Data.Width = props.width;
+        m_Data.Height = props.height;
+
+        if (!s_GLFWInitialized)
+        {
+            if (!glfwInit())
+            {
+                throw std::runtime_error("Failed to initialize GLFW");
+            }
+            glfwSetErrorCallback(GLFWErrorCallback);
+            s_GLFWInitialized = true;
+        }
+
+        glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+        glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 2);
+        glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+        glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+
+        AE_CORE_INFO("[1/2] - GLFW Initialized.");
+
+        m_Window = glfwCreateWindow((int)props.width, (int)props.height, props.title.c_str(), nullptr, nullptr);
+        glfwMakeContextCurrent(m_Window);
+
+        if (!gladLoadGLLoader(reinterpret_cast<GLADloadproc>(glfwGetProcAddress)))
+        {
+            throw std::runtime_error("Failed to initialize GLAD");
+        }
+
+        AE_CORE_INFO("[2/2] - GLAD Initialized.");
+
+        glfwSetWindowUserPointer(m_Window, &m_Data);
+        SetVSync(true);
+
+        // Set GLFW callbacks
+        glfwSetWindowSizeCallback(m_Window, [](GLFWwindow *window, int width, int height)
+                                  {
+            WindowData& data = *static_cast<WindowData*>(glfwGetWindowUserPointer(window));
+            data.Height = height;
+            data.Width = width;
+
+            Events::WindowResizeEvent event(width, height);
+            data.EventCallback(event); });
+
+        glfwSetWindowCloseCallback(m_Window, [](GLFWwindow *window)
+                                   {
+            const WindowData& data = *static_cast<WindowData*>(glfwGetWindowUserPointer(window));
+            Events::WindowCloseEvent event;
+            data.EventCallback(event); });
+
+        glfwSetKeyCallback(m_Window, [](GLFWwindow *window, int key, int scancode, int action, int mods)
+                           {
+            const WindowData& data = *static_cast<WindowData*>(glfwGetWindowUserPointer(window));
+            switch (action)
+            {
+            case GLFW_PRESS:
+                {
+                    Events::KeyPressedEvent event(key);
+                    data.EventCallback(event);
+                    break;
+                }
+            case GLFW_RELEASE:
+                {
+                    Events::KeyReleasedEvent event(key);
+                    data.EventCallback(event);
+                    break;
+                }
+            case GLFW_REPEAT:
+                {
+                    Events::KeyPressedEvent event(key);
+                    data.EventCallback(event);
+                    break;
+                }
+            default: break;
+            } });
+
+        glfwSetCharCallback(m_Window, [](GLFWwindow *window, const unsigned int keycode)
+                            {
+            const WindowData& data = *static_cast<WindowData*>(glfwGetWindowUserPointer(window));
+            Events::KeyTypedEvent event(keycode);
+            data.EventCallback(event); });
+
+        glfwSetMouseButtonCallback(m_Window, [](GLFWwindow *window, int button, int action, int mods)
+                                   {
+            const WindowData& data = *static_cast<WindowData*>(glfwGetWindowUserPointer(window));
+            switch (action)
+            {
+            case GLFW_PRESS:
+                {
+                    Events::MouseButtonPressedEvent event(button);
+                    data.EventCallback(event);
+                    break;
+                }
+            case GLFW_RELEASE:
+                {
+                    Events::MouseButtonReleasedEvent event(button);
+                    data.EventCallback(event);
+                    break;
+                }
+            default: break;
+            } });
+
+        glfwSetScrollCallback(m_Window, [](GLFWwindow *window, const double xOffset, const double yOffset)
+                              {
+            const WindowData& data = *static_cast<WindowData*>(glfwGetWindowUserPointer(window));
+            Events::MouseScrolledEvent event((float)xOffset, (float)yOffset);
+            data.EventCallback(event); });
+
+        glfwSetCursorPosCallback(m_Window, [](GLFWwindow *window, double xPos, double yPos)
+                                 {
+            WindowData& data = *static_cast<WindowData*>(glfwGetWindowUserPointer(window));
+            Events::MouseMovedEvent event((float)xPos, (float)yPos);
+            data.EventCallback(event); });
     }
 
     void LinuxWindow::OnUpdate()
@@ -23,7 +159,7 @@ namespace AE::Platform::Linux
         glfwSwapBuffers(m_Window);
     }
 
-    void LinuxWindow::SetVSync(bool enabled)
+    void LinuxWindow::SetVSync(const bool enabled)
     {
         if (enabled)
             glfwSwapInterval(1);
@@ -34,9 +170,4 @@ namespace AE::Platform::Linux
     }
 
     bool LinuxWindow::IsVSync() const { return m_Data.VSync; }
-
-    void LinuxWindow::Init(const WindowProps &props)
-    {
-    }
-
 }
